15dbe9456ee14a82a72b1c5ab3382f3150cbf549
jml0405 <juanlopera0405@gmail.com>
2025-01-23 14:20:10 -0400

    arreglado modelo

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  favoritos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Libro' }], // Relación con los libros
+const usuarioSchema = new mongoose.Schema({
+  Nombre: { type: String, required: true }, // Nombre de usuario
+  Correo: { type: String, required: true, unique: true }, // Correo electrónico
+  Contraseña: { type: String, required: true }, // Contraseña del usuario
+  Telefono: { type: String, required: false }, // Teléfono del usuario
+  Imagen: { type: String, default: '' }, // Imagen del usuario
+  Favoritos: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Libro' }], // Relación con los libros favoritos
 });
 
-module.exports = mongoose.model('Usuario', userSchema);
-
+module.exports = mongoose.model('Usuario', usuarioSchema);

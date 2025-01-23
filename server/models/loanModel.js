f76e8d55b660dc729a2c52b863676b1c48f916a9
MrGabox <gabriel.alba01@gmail.com>
2025-01-23 14:35:35 -0400

    Codigo en prestamo

--- server/models/loanModel.js
+++ server/models/loanModel.js
@@ -4,39 +4,51 @@ const prestamoSchema = new mongoose.Schema({
   ID_Estudiante: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'Usuario', 
-    required: true // Un préstamo pertenece a un usuario
+    required: true 
   },
   ID_Libro: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'Libro', 
-    required: true // Un préstamo pertenece a un libro
+    required: true 
   },
   Fecha_Prest: { 
     type: Date, 
-    default: Date.now // Fecha de préstamo asignada automáticamente
+    default: Date.now 
   },
   Fecha_Devol: { 
     type: Date, 
     default: function () {
-      return new Date(Date.now() + 7 * 24 * 60 * 60 * 1000); // Fecha de devolución 7 días después
+      return new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
     }
   },
   ID_Estado: { 
     type: mongoose.Schema.Types.ObjectId, 
     ref: 'EstadoPrestamo', 
-    required: true // Un préstamo tiene un estado específico
+    required: true 
   },
   MetodoPago: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'MetodoPago',
-    required: true // El método de pago es obligatorio
+    required: true 
   },
   Monto: {
     type: Number,
-    required: true, // Monto del préstamo
-    min: 0 // Validación: no puede ser negativo
+    required: true,
+    min: 0
+  },
+  Codigo: { 
+    type: String, 
+    required: true, 
+    unique: true 
   }
 });
 
-module.exports = mongoose.model('Prestamo', prestamoSchema);
+// Pre-hook para generar el Código antes de la validación
+prestamoSchema.pre('validate', function (next) {
+  if (!this.Codigo) {
+    this.Codigo = Math.floor(100000 + Math.random() * 900000).toString(); // Genera un número aleatorio de 6 dígitos
+  }
+  next();
+});
 
+module.exports = mongoose.model('Prestamo', prestamoSchema);
